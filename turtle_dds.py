0426587c529df3ed25750bc73b432df46da0ba71
WolfFromFog <stas.povelitsin@yandex.ru>
2026-05-11 18:38:11 +0700

    Откта

--- turtle_dds.py
+++ turtle_dds.py
@@ -3,15 +3,8 @@ from cyclonedds.idl import IdlStruct
 
 @dataclass
 class TurtlePose(IdlStruct):
-    """Позиция черепахи."""
+    """Сообщение с позицией черепахи."""
     id: int
     x: float
     y: float
     theta: float
-
-@dataclass
-class TurtleCmd(IdlStruct):
-    """Команда управления (линейная и угловая скорость)."""
-    id: int          # идентификатор черепахи, которой предназначена команда
-    linear: float
-    angular: float
